4aced4e46ce02b3055f514f02485f91c9b21a628
Charlton Callender <chacalle@uw.edu>
2020-05-26 00:03:33 -0700

    inherit rinvgamma parameters and add example

--- R/invgamma.R
+++ R/invgamma.R
@@ -3,14 +3,12 @@
 #' @description Random generation for the Inverse Gamma distribution with
 #'   parameters `shape` (alpha) and `scale` (beta).
 #'
-#' @param n number of observations. If length(n) > 1, the length is taken to be
-#'   the number required.
-#' @param shape shape parameter of Inverse Gamma distribution, also known as the
-#'   alpha parameter. Must be positive.
-#' @param scale scale parameter of Inverse Gamma distribution, also known as the
-#'   beta parameter. Must be positive.
+#' @inheritParams stats::rgamma
 #'
 #' @return numeric vector of length \code{n}.
+#'
+#' @example
+#' rinvgamma(10, 0.5, 0.01)
 #'
 #' @export
 rinvgamma <- function(n, shape, scale) {
