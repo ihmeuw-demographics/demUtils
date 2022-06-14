74a6ab0ba7d0819f79d6b268783213556ad8ce5c
Charlton Callender <chacalle@uw.edu>
2022-06-14 14:51:45 -0700

    remove 'combine_pdfs' function

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-# as explained here https://cran.r-project.org/web/packages/reticulate/vignettes/package.html
-# https://cran.r-project.org/web/packages/reticulate/vignettes/python_dependencies.html
-# https://github.com/rstudio/reticulate/issues/337#issuecomment-563077433
-
-.onLoad <- function(libname, pkgname) {
-
-  reticulate::configure_environment(pkgname, force = TRUE)
-
-}
